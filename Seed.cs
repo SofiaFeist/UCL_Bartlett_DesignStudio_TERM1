023637d285417f518df64438a3e1097f493f1a20
Sofia Feist <SofiaFeist@users.noreply.github.com>
2019-11-25 00:59:31 +0000

    Added Seed class script

---
+++ Seed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seed 
+{
+    public Vector3 Position = new Vector3(0,0,0);
+
+
+
+    public Seed(GameObject prefab, Vector3 position, Material material)
+    {
+        this.Position = position;
+        GameObject seed = Object.Instantiate(prefab, Position, Quaternion.identity);
+        seed.GetComponent<Renderer>().material = material;
+    }
+}
